254b480b9c5668b1208703909578b01b767a42eb
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-15 17:27:08 -0400

    feat: created service to get all categories

---
+++ src/services/GetAllCategoriesService.ts
@@ -0,0 +1,10 @@
+import dataSource from "../database/dataSource";
+import {Category} from "../entities/Category";
+
+export class GetAllCategoriesService {
+    async execute(): Promise<Category[]> {
+        const categoryRepository = dataSource.getRepository(Category);
+
+        return categoryRepository.find();
+    }
+}
