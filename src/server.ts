caa7410b1020dd95fed4d32087741f87424ce65f
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-15 14:49:34 -0400

    feat: added routes to be used

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import "reflect-metadata"
 import express from "express"
 import "./database/dataSource";
+import {routes} from "./routes";
+
+const PORT = process.env.PORT || 3000
 
 const app = express()
-const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
