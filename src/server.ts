5355bafffd1ef849f5af171421698d46d0357e1c
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-07-15 14:38:47 -0400

    refactor: rename file datasource to dataSource, and replace import on server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"
 import express from "express"
-import "./database/datasource";
+import "./database/dataSource";
 
 const app = express()
 const PORT = process.env.PORT || 3000
